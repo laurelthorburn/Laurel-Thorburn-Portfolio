dfb4242a2937d8ae47a72fc596f1c4798b80be4d
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-25 12:47:42 -0700

    Added to intro

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -3,7 +3,19 @@ import './intro.css';
 const Intro = () => {
     return (
         <div className ="intro">
-            <div className="intro-left">leftie</div>
+            <div className="intro-left">
+                <div className="intro-left-wrapper">
+                    <h2>Hello, my name is</h2>
+                    <h1 className="intro-name"></h1>
+                    <div className="intro-title">
+                        <div className="intro-title-wrapper">
+                            <div className="intro-title-item">Full Stack Developer</div>
+                            <div className="intro-title-item">Audiologist</div>
+                            <div className="intro-title-item">Lover of Donuts</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="intro-right">rightie</div>
         </div>
     )
