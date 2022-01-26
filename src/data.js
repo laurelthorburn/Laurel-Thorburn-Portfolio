5edb40203ab84efbbfe9980cdba6ea104703233b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-25 18:37:51 -0700

    Added comign soon imgs to data

--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export const projects = [
     {
         id: 1,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
     {
         id: 2,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
     {
         id: 3,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
     {
         id: 4,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
     {
         id: 5,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
     {
         id: 6,
-        img: "",
+        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
         link: "https://github.com/laurelthorburn"
     },
 ];
