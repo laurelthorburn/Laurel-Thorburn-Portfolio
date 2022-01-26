00aa4045f924dbbcb59ae0a6197a706df43a81f0
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-26 12:13:50 -0700

    Styling contact icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,9 @@
 import './contact.css';
+import Phone from '../../media/phone.png';
+import Location from '../../media/needle.png';
+import Email from '../../media/letter.png';
+import LinkedIn from '../../media/linkedin.png';
+import Github from '../../media/github.png';
 
 const Contact = () => {
     return (
@@ -10,7 +15,16 @@ const Contact = () => {
                             <h1 className="contactTitle"> Contact Information:</h1>
                             <div className="contactInfo">
                                 <div className="contactInfoItem">
-                                    <img src="" alt="" className="contactIcon" />
+                                    <img src={Phone} alt="Phone Icon" className="contactIcon" />
+                                + 555-555-5555
+                                </div>
+                                <div className="contactInfoItem">
+                                    <img src={Email} alt="Email Icon" className="contactIcon" />
+                                codinglaurel@gmail.com
+                                </div>
+                                <div className="contactInfoItem">
+                                    <img src={Location} alt="Location Icon" className="contactIcon" />
+                                Aurora, CO, United States
                                 </div>
                             </div>
                         </div>
