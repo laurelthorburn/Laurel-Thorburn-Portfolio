0a3660a94ba10cb11c887a5dd42fc338e07d5611
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-26 12:37:27 -0700

    Fixed bug causing contact left/right to only take up 50% of entire screen instead of each having 50%

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,41 +8,39 @@ import Github from '../../media/github.png';
 const Contact = () => {
     return (
 
-            <div className="contact">
-                <div className="contactBg">
-                    <div className="contactWrapper">
-                        <div className="contact-left">
-                            <h1 className="contactTitle"> Contact Information:</h1>
-                            <div className="contactInfo">
-                                <div className="contactInfoItem">
-                                    <img src={Phone} alt="Phone Icon" className="contactIcon" />
-                                + 555-555-5555
-                                </div>
-                                <div className="contactInfoItem">
-                                    <img src={Email} alt="Email Icon" className="contactIcon" />
-                                codinglaurel@gmail.com
-                                </div>
-                                <div className="contactInfoItem">
-                                    <img src={Location} alt="Location Icon" className="contactIcon" />
-                                Aurora, CO, United States
-                                </div>
+        <div className="contact">
+                <div className="contactWrapper">
+                    <div className="contact-left">
+                        <h1 className="contactTitle"> Contact Information:</h1>
+                        <div className="contactInfo">
+                            <div className="contactInfoItem">
+                                <img src={Phone} alt="Phone Icon" className="contactIcon" />
+                            + 555-555-5555
+                            </div>
+                            <div className="contactInfoItem">
+                                <img src={Email} alt="Email Icon" className="contactIcon" />
+                            codinglaurel@gmail.com
+                            </div>
+                            <div className="contactInfoItem">
+                                <img src={Location} alt="Location Icon" className="contactIcon" />
+                            Aurora, CO, United States
                             </div>
                         </div>
-                        <div className="contact-right">
-                            <p className="contactDesc">
-                                <b>Let's work together.</b> Send me an email so we can discuss how I can contribute to your team.
-                            </p>
-                            <form>
-                                <input type="text" placeholder='Name' name='{user_name}' />
-                                <input type="text" placeholder='Subject' name='{user_subject}' />
-                                <input type="text" placeholder='Email' name='{user_email}' />
-                                <textarea name="message" rows="5" placeholder='Message'></textarea>
-                                <button>Submit!</button>
-                            </form>
-                        </div>
+                    </div>
+                    <div className="contact-right">
+                        <p className="contactDesc">
+                            <b>Let's work together.</b> Send me an email so we can discuss how I can contribute to your team.
+                        </p>
+                        <form>
+                            <input type="text" placeholder='Name' name='{user_name}' />
+                            <input type="text" placeholder='Subject' name='{user_subject}' />
+                            <input type="text" placeholder='Email' name='{user_email}' />
+                            <textarea name="message" rows="5" placeholder='Message'></textarea>
+                            <button>Submit!</button>
+                        </form>
                     </div>
                 </div>
-            </div>
+        </div>
 
     )
 };
