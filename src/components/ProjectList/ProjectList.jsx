b799fac8cae4b9a106bd8fb0270920657a4310eb
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-25 18:08:03 -0700

    Creating aboutme template

---
+++ src/components/ProjectList/ProjectList.jsx
@@ -0,0 +1,17 @@
+import './productList.css';
+
+export const ProjectList = () => {
+    return(
+        <div className="projectList">
+            <h1 className="productListTitle">
+                Watch Me Work, Watch Me NayNay
+            </h1>
+            <p className="productListDesc">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam dolores harum magnam ea, provident commodi eveniet voluptates libero excepturi exercitationem odit aliquam officia blanditiis, illo hic. Officia repellat culpa quasi error nulla dolor, delectus dolorum reiciendis iure laborum vero dignissimos tenetur iusto veniam, sit doloribus nisi cumque quaerat esse ea.
+            </p>
+
+        </div>
+        );
+};
+
+export default ProjectList
