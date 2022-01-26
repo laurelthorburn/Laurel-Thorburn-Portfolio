7eb7716970a66ce830e2328ae1278eaa4803ad5d
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-25 17:58:34 -0700

    Styling about me page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,8 +3,15 @@ import './about.css';
 const About = () => {
     return (
         <div className="about">
-           <div className="about-left">leftie</div>
-           <div className="about-right">rightie</div>
+           <div className="about-left">
+               This will have stuff soon...
+           </div>
+           <div className="about-right">
+               <h1 className="about-title">All About Me:</h1>
+               <p className="about-desc">
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti numquam non ut! Nostrum sapiente repellendus veniam minus reiciendis autem nesciunt itaque, nemo ex. Magnam illo minus inventore officiis molestias animi atque laborum rem soluta ducimus consequuntur nesciunt reprehenderit quo quasi similique, ratione minima ea fuga, est beatae, quidem labore doloribus.
+               </p>
+           </div>
         </div>
     )
 };
