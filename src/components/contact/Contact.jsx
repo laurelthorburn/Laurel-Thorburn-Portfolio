667df24fb9302b433ac7877e5a4205938083be4f
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-26 09:51:35 -0700

    Styling Contact Page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,21 @@ import './contact.css';
 const Contact = () => {
     return (
         <div>
-            Contact Me :)
+            <div className="contact">
+                <div className="contactBg">
+                    <div className="contactWrapper">
+                        <div className="contact-left">
+                            <h1 className="contactTitle"> Contact Information:</h1>
+                            <div className="contactInfo">
+                                <div className="contactInfoItem">
+                                    <img src="" alt="" className="contactIcon" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="contact-right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
