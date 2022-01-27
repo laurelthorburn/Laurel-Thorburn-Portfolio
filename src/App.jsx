b0da035d2920afe1e939e13b47b7331ea7233d87
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-27 12:10:44 -0700

    Removed toggle and added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
-import { ThemeContext } from './context';
 import Intro from './components/intro/Intro';
 import ProjectList from './components/projectList/ProjectList';
-import Toggle from './components/toggle/Toggle';
+import Navbar from './components/navbar/Navbar';
 
 
 const App = () => {
 
-  const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
-
   return (
 
-    <div style={{ backgroundColor:darkMode ? "#222" : "white", color: darkMode && "white"}}>
-      <Toggle />
+    <div>
+      <Navbar />
       <Intro />
       <About />
       <ProjectList />
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css';
+
+export const Navbar = (props) => {
+  return (
+    <div>
+        Future navigation bar
+    </div>
+    )
+};
+
+export default Navbar;
