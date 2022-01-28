4b1d7ae0561940357f7b9e5409ec2d91bb165291
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-28 12:30:34 -0700

    Added important to magins as universal margin and padding was overriding later css margins

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -3,11 +3,6 @@ import './project.css'
 const Project = ({img,link}) => {
     return (
         <div className="project">
-            <div className="projectBrowser">
-                <div className="projectCircle"></div>
-                <div className="projectCircle"></div>
-                <div className="projectCircle"></div>
-            </div>
             <a href={link} target="_blank" rel="noreferrer">
                 <img src={img} alt="" className="projectImg" />
             </a>
