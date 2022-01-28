389a9b038f51435e525e400c3de569823e292dd6
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-28 12:52:38 -0700

    Made cards transform on hover

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,11 +1,19 @@
 import './project.css'
 
-const Project = ({img,link}) => {
+const Project = ({img,link,title}) => {
     return (
         <div className="project">
+            <div className="card">
+            <figure className="front">
             <a href={link} target="_blank" rel="noreferrer">
-                <img src={img} alt="" className="projectImg" />
+                <img src={img} alt={title} className="projectImg" />
             </a>
+            </figure>
+
+            <figure className="back">
+                This will have the github link and the deployed site link :)
+            </figure>
+            </div>
         </div>
     )
 };
--- src/data.js
+++ src/data.js
@@ -2,31 +2,37 @@ export const projects = [
     {
         id: 1,
         img: 'https://i.ibb.co/hVcBVCG/donutsite.png',
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
+        title: "Donut Forget to Review"
     },
     {
         id: 2,
         img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Team-Profile-Generator",
+        tile: "Team Profile Generator"
     },
     {
         id: 3,
         img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Weather-Dashboard", 
+        title: "Weather Dashboard"
     },
     {
         id: 4,
         img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Note-Taker", 
+        title: "Note Taker"
     },
     {
         id: 5,
         img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Text-Editor", 
+        title: "Text Editor"
     },
     {
         id: 6,
         img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
-        link: "https://github.com/laurelthorburn"
+        link: "https://github.com/laurelthorburn/Tech-Blog", 
+        title: "Tech Blog"
     },
 ];
