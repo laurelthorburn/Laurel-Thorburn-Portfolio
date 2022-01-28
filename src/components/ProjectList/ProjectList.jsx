4d1f4fdce6775ac62a462ea4438a49a74fe19a66
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-28 12:22:47 -0700

    Made project cards smaller and changed to space evenely instead of space between

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -5,14 +5,7 @@ import {projects} from '../../data';
 export const ProjectList = () => {
     return(
         <div className="projectList">
-            <div className="projectListTexts">
-                <h1 className="projectListTitle">
-                    Watch Me Work, Watch Me NayNay
-                </h1>
-                <p className="projectListDesc">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam dolores harum magnam ea, provident commodi eveniet voluptates libero excepturi exercitationem odit aliquam officia blanditiis, illo hic. Officia repellat culpa quasi error nulla dolor, delectus dolorum reiciendis iure laborum vero dignissimos tenetur iusto veniam, sit doloribus nisi cumque quaerat esse ea.
-                </p>
-            </div>
+
             <div className="projectListContent">
             {projects.map((project) => (
                 <Project key={project.id} img={project.img} link={project.link}/>
