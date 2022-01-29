35a152b0b099c44fae930ee1ce6c30808596466f
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-29 08:06:35 -0700

    Added img project links

--- src/data.js
+++ src/data.js
@@ -8,35 +8,35 @@ export const projects = [
     },
     {
         id: 2,
-        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
+        img: "https://i.ibb.co/Z10krcJ/teamgen.png",
         ghLink: "https://github.com/laurelthorburn/Team-Profile-Generator",
         siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio",
         title: "Team Profile Generator"
     },
     {
         id: 3,
-        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
+        img: "https://i.ibb.co/7p9SvTJ/weatherdash.png",
         ghLink: "https://github.com/laurelthorburn/Weather-Dashboard",
         siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
         title: "Weather Dashboard"
     },
     {
         id: 4,
-        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
+        img: "https://i.ibb.co/hykxbTt/notetaker.png",
         ghLink: "https://github.com/laurelthorburn/Note-Taker",
         siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
         title: "Note Taker"
     },
     {
         id: 5,
-        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
+        img: "https://i.ibb.co/sjfmTBW/texted.png",
         ghLink: "https://github.com/laurelthorburn/Text-Editor",
         siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
         title: "Text Editor"
     },
     {
         id: 6,
-        img: "https://cdn.pixabay.com/photo/2017/02/16/02/54/coming-soon-2070393_960_720.jpg",
+        img: "https://i.ibb.co/3Yw1dnx/techblog.png",
         ghLink: "https://github.com/laurelthorburn/Tech-Blog",
         siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
         title: "Tech Blog"
