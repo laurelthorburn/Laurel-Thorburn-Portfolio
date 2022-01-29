76c707a89426dbda0f04033a64b561a561ef3d75
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-29 07:19:14 -0700

    Styling back of card, changed design from circles to recangular div's

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -1,5 +1,7 @@
 import { useContext } from 'react';
 import './project.css'
+import Github from '../../media/github.png'
+import Computer from '../../media/computer.png'
 
 const Project = ({img,link,title}) => {
     console.log(link)
@@ -16,6 +18,14 @@ const Project = ({img,link,title}) => {
             <figure className="back">
                 <div className="cardTitle">
                     Title: {title}
+                    </div>
+                    <div className="githubBlock">
+                    View on Github 
+                    {/* <img src={Github} alt="View Github Icon" className="cardIcon" /> */}
+                    </div>
+                    <div className="siteBlock">
+                        View Site
+                    {/* <img src={Computer} alt="View Site Icon" className="cardIcon" /> */}
                 </div>
             </figure>
             </div>
