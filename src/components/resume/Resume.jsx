6d3ddc08aff281a117a5d4ab0b11788f0f289155
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-30 08:25:54 -0700

    Creating resume sections

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -3,7 +3,17 @@ import './resume.css'
 const Resume = () => {
     return(
         <div>
-            Yo I am your resume
+            <div className="resume-left">
+                <div className="resumeName">
+                    LAUREL THORBURN, AuD
+                    <span className="resumeCatch">SOFTWARE ENGINEER, AUDIOLOGIST, DONUT LOVER</span>
+                </div>
+                <div className="resumeWork"></div>
+                <div className="resumeEducation"></div>
+            </div>
+            <div className="resume-right">
+                <div className="resumeContact"></div>
+            </div>
         </div>
     )
 }
