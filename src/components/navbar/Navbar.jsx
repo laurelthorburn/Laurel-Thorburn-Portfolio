27532727df863ec0b2b6b0e0231011637bc772e7
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-30 07:43:19 -0700

    Added active link decor to display current open page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar({ currentPage, handlePageChange }) {
                     onClick={() => handlePageChange('Intro')}
                     // This is a conditional (ternary) operator that checks to see if the current page is "Intro"
                     // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-                    className={currentPage === 'Intro' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === 'Intro' ? 'nav-link active currentLink' : 'nav-link'}
                     >
                     About Me
                     </a>
@@ -27,7 +27,7 @@ function Navbar({ currentPage, handlePageChange }) {
                     href="#contact"
                     onClick={() => handlePageChange('Contact')}
                     // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === 'Contact' ? 'nav-link active currentLink' : 'nav-link'}
                     >
                     Contact
                     </a>
@@ -37,7 +37,7 @@ function Navbar({ currentPage, handlePageChange }) {
                     href="#projectList"
                     onClick={() => handlePageChange('ProjectList')}
                     // Check to see if the currentPage is `ProjectList`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === 'ProjectList' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === 'ProjectList' ? 'nav-link active currentLink' : 'nav-link'}
                     >
                     Projects
                     </a>
@@ -47,7 +47,7 @@ function Navbar({ currentPage, handlePageChange }) {
                     href="#resume"
                     onClick={() => handlePageChange('Resume')}
                     // Check to see if the currentPage is `ProjectList`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                    className={currentPage === 'Resume' ? 'nav-link active currentLink' : 'nav-link'}
                     >
                     Resume
                     </a>
