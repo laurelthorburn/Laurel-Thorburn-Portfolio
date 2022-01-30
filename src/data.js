ff97fdfcc16ed144d1fb300e19b5e37b1cc0e726
Laurel Thorburn <laurelthorburn@gmail.com>
2022-01-30 07:56:38 -0700

    Updagted data deployed links for projects and centered imgs in cards

--- src/data.js
+++ src/data.js
@@ -2,43 +2,43 @@ export const projects = [
     {
         id: 1,
         img: 'https://i.ibb.co/xJCCWtb/donutsite.png',
-        ghLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio",
+        ghLink: "https://github.com/laurelthorburn/DONUT-FORGET-TO-REVIEW", 
+        siteLink: "https://donut-forget-to-review.herokuapp.com/",
         title: "Donut Forget to Review"
     },
     {
         id: 2,
         img: "https://i.ibb.co/Z10krcJ/teamgen.png",
         ghLink: "https://github.com/laurelthorburn/Team-Profile-Generator",
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio",
+        siteLink: "https://laurelthorburn.github.io/Team-Profile-Generator/",
         title: "Team Profile Generator"
     },
     {
         id: 3,
         img: "https://i.ibb.co/7p9SvTJ/weatherdash.png",
         ghLink: "https://github.com/laurelthorburn/Weather-Dashboard",
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
+        siteLink: "https://laurelthorburn.github.io/Weather-Dashboard/", 
         title: "Weather Dashboard"
     },
     {
         id: 4,
         img: "https://i.ibb.co/hykxbTt/notetaker.png",
         ghLink: "https://github.com/laurelthorburn/Note-Taker",
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
+        siteLink: "https://morning-reaches-98085.herokuapp.com/", 
         title: "Note Taker"
     },
     {
         id: 5,
         img: "https://i.ibb.co/sjfmTBW/texted.png",
         ghLink: "https://github.com/laurelthorburn/Text-Editor",
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
+        siteLink: "https://laurels-text-editor.herokuapp.com/", 
         title: "Text Editor"
     },
     {
         id: 6,
         img: "https://i.ibb.co/3Yw1dnx/techblog.png",
         ghLink: "https://github.com/laurelthorburn/Tech-Blog",
-        siteLink: "https://github.com/laurelthorburn/Laurel-Thorburn-Portfolio", 
+        siteLink: "https://laurels-tech-babble.herokuapp.com/", 
         title: "Tech Blog"
     },
 ];
