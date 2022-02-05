9d684abef63e64be1d0330030c43efb02b583149
Laurel Thorburn <laurelthorburn@gmail.com>
2022-02-05 09:16:40 -0700

    Removed card styling on project cards

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -7,27 +7,26 @@ const Project = ({img,ghLink,siteLink,title}) => {
             <div className="card">
             <figure className="front">
                 <img src={img} alt={title} className="projectImg" />
-                <div className="cardTitle">
-                    {title}
-                </div>
-            </figure>
-
-            <figure className="back">
-                    <a href={ghLink} target="_blank" rel="noreferrer" className='projectLink'>
-                        <div className="githubBlock" onClick="location.href={ghLink}">
-                            <a href={ghLink} target="_blank" rel="noreferrer" className='projectLink'>
-                                View on Github
-                            </a>
-                        </div>
-                    </a>
-
-                    <a href={siteLink} target="_blank" rel="noreferrer" className='siteLink'>
-                        <div className="siteBlock">
-                            <a href={siteLink} target="_blank" rel="noreferrer" className='siteLink'>
-                                View Site
+                <div className="projectLinks">
+                    <div className="cardTitle">
+                        Title: {title}
+                    </div>
+                        <a href={ghLink} target="_blank" rel="noreferrer" className='projectLink'>
+                            <div className="githubBlock" onClick="location.href={ghLink}">
+                                <a href={ghLink} target="_blank" rel="noreferrer" className='projectLink'>
+                                    View on Github
                                 </a>
-                        </div>
-                    </a>
+                            </div>
+                        </a>
+
+                        <a href={siteLink} target="_blank" rel="noreferrer" className='siteLink'>
+                            <div className="siteBlock">
+                                <a href={siteLink} target="_blank" rel="noreferrer" className='siteLink'>
+                                    View Site
+                                    </a>
+                            </div>
+                        </a>
+                    </div>
             </figure>
             </div>
         </div>
